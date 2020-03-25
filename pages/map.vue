ba4563390d5b3225cbe3348553e757a817910b6e
Jun Wang <wang208@indiana.edu>
2020-03-24 22:28:21 -0400

    interface imported

--- pages/map.vue
+++ pages/map.vue
@@ -26,7 +26,44 @@
                 ETAG Map Tool
                 <div class="leaflet-sidebar-close"><i class="fa fa-caret-left"></i></div>
             </h1>
-            <p>A responsive sidebar for mapping libraries</p>
+                <br>
+                <!-- data type -->
+                <b-form-group label="Data Type" label-size="lg" class="font-weight-bold">
+                <b-form-radio-group size="lg" id="radio-group-2"  @change="datatype_onChange"  name="radio-sub-component" stacked>
+                <b-form-radio  value="readers" >Readers</b-form-radio>
+                <b-form-radio value="tags">Tag Reads</b-form-radio>
+                </b-form-radio-group>
+                </b-form-group>
+                <!-- display type -->
+                <b-form-group label="Display Type" label-size="lg" class="font-weight-bold">
+                <b-form-radio-group size="lg" id="opt_displaytype"  @change="displaytype_onChange"  name="opt_displaytype" stacked>
+                <b-form-radio  value="tag_summaries" checked="checked">Summaries</b-form-radio>
+                <b-form-radio value="raw_tag_reads">Raw tag reads</b-form-radio>
+                </b-form-radio-group>
+                </b-form-group>
+                <!-- display options -->
+                <h4>Filters</h4>
+                <div class="border-top my-3"></div>
+                <h6>Species</h6>
+                <select v-model="selected" class="selectpicker"  ref='select1'  id="species_selector" title="Choose one or more..." data-live-search="true" multiple data-actions-box="true">
+                    <option v-for="option in allspecies" v-bind:value="option.value">
+                        {{ option.text }}
+                    </option>
+                </select>
+
+                <h6>Tag ID</h6>
+                          <select v-model="selected" class="selectpicker" id="tag_selector" title="Choose one or more..." data-live-search="true" multiple data-actions-box="true">
+                    <option v-for="option in alltagid" v-bind:value="option.value">
+                        {{ option.text }}
+                    </option>
+                    </select>
+                <h6>Data Privacy</h6>
+                            <select v-model="selected" class="selectpicker" id="data_privacy" title="User only">
+                              <option>User only</option>
+                              <option>All data</option>
+                </select>
+                <br><div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+
         </div>
 
         <div class="leaflet-sidebar-pane" id="messages">
@@ -52,6 +89,11 @@ import 'leaflet-sidebar-v2';
 import 'leaflet-sidebar-v2/css/leaflet-sidebar.css';
 import "@fortawesome/fontawesome-free";
 import '@fortawesome/fontawesome-free/css/all.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap-select';
+import 'bootstrap-select/dist/css/bootstrap-select.min.css';
+
   export default {
     auth: false, // do not require to be logged in to view this page
     mounted() {
@@ -62,6 +104,23 @@ import '@fortawesome/fontawesome-free/css/all.css';
     data () {
       return {
         center: [35.2059, -97.4457], // Coordinates for University of Oklahoma
+        selected: [],
+        datatype_sel: '',
+        allspecies:[
+              {text:'Cardinal', value: 'Cardinal' },
+              {text:'Blue Jay', value: 'Blue Jay' },
+              {text:'Chickadee', value: 'Chickadee' },
+              {text:'Titmouse', value: 'Titmouse' },
+              {text:'Sparrow', value: 'Sparrow' },
+              {text:'Finch', value: 'Finch' },
+              {text:'Others', value: 'Others' }
+        ],
+        alltagid:[
+              {text:'620000620', value: '620000620'},
+              {text:'620000731', value: '620000731'},
+              {text:'06200005BA', value: '06200005BA'},
+              {text:'TDP000064D', value: 'TDP000064D'}
+        ],
       }
     },
     methods: {
