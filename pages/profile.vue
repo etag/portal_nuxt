67bb0dc449c2883d659ebeea0e550251db65e571
Tyler Pearson <tdpearson@ou.edu>
2020-04-01 18:02:52 -0500

    populate profile page

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    Profile
+
+    <!-- TODO: add styling and a form for updating -->
+
+    <div> Avatar: <img :src="profile.gravatar" alt="Avatar Image"></div>
+    <div> Avatar Link: {{ profile.gravatar }}</div>
+    <div> Username: {{ profile.username }} </div>
+    <div> First Name: {{ profile.firstname }} </div>
+    <div> Last Name: {{ profile.lastname }} </div>
+    <div> Email: {{ profile.email }} </div>
+
   </div>
 </template>
 
@@ -8,6 +17,17 @@
 
 export default {
   components: {
+  },
+  computed: {
+    profile () {
+      return {
+        username: this.$store.$auth.user.username,
+        firstname: this.$store.$auth.user.first_name,
+        lastname: this.$store.$auth.user.last_name,
+        email: this.$store.$auth.user.email,
+        gravatar: this.$store.$auth.user.gravator_url, // TODO: fix "gravator" typo on API end-point
+      }
+    },
   },
 }
 </script>
