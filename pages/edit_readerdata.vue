bcf27653f7f612177fe51cc794eb074fa7b57e7e
Edsel Brian Norwood II <edselnorwood83@gmail.com>
2020-04-20 11:35:19 -0400

    added v-for in forms

--- pages/edit_readerdata.vue
+++ pages/edit_readerdata.vue
@@ -1,31 +1,88 @@
 <template>
-<div>
-  <b-form @submit="saveChanges">
-    {{ item.url }}
-  </b-form>
-</div>
+  <div>
+    <b-card bg-variant="light">
+      <b-form-group>
+        <b-form @submit.prevent="saveChanges" v-model=form>
+
+          <b-form-group id="Title" :label="'Editing User ID: ' + item.user_id"></b-form-group>
+          
+          <b-form-group id="input-group-1" label="Reader_ID" label-for="input-1">
+            <b-form-input
+              id="Reader_ID"
+              :value="item.reader_id"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="input-group-2" label="Description" label-for="input-2">
+            <b-form-input
+              id="Description"
+              :value="item.description"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="input-group-3" label=fetchLoc_id label-for="input-3">
+            <b-form-input
+              id="input-2"
+              :value="coordinates.lat"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-button type="submit" variant="primary">Submit</b-button>
+          <b-button type="button" variant="primary" @click="$router.go(-1)">Cancel</b-button>
+          
+        </b-form>
+      </b-form-group>
+      <b-button type="button" variant="primary" @click.prevent="fetchLoc_id">test</b-button>
+    </b-card>
+
+  </div>
 </template>
 
 <script>
   export default {
+    async created() {
+      var self=this
+      let { reader_data } = this.$axios.get('/api/etag/reader_location/?reader_id=' + this.item.reader_id + '&format=json',{
+        headers: {Authorization: this.$auth.$storage._state['_token.local']}
+      }).then(function (reader_data, vm){
+        let { location_data } = self.$axios.get('/api/etag/locations/?location_id=' + reader_data.data.results[0].location_id + '&format=json'
+          ).then(function (location_data) {
+            this.coordinates.lat = location_data.data.results[0].latitude;
+            this.coordinates.lon = location_data.data.results[0].longitude;
+      })
+      });
+    },
     computed: {
       item() {
         return this.$store.state.readers.activeItem
       },
+      form: {
+          Reader_ID: '',
+          Description: '',
+      },
+      coordinates: {
+        lat: '',
+        lon: '',
+      }
     },
     methods: {
-      saveChanges() {
+      saveChanges(evt) {
+        evt.preventDefault()
         // TODO: add axios patch to submit edits
         this.$axios({
           url: this.item.url,
-          method: 'PATCH',
-          data: fields,
+          method: 'patch',
+          data: this.$item,
           headers: {Authorization: this.$auth.$storage._state['_token.local']}
-        }).then((response) => {
-          // check if successful and reroute to taggeddata page
-          this.$router.push('readerdata')
+        }).then(function (response) {
+            //this.$router.push('taggeddata')
+            console.log(response);
         })
-      }
+        .catch(function (response) {
+            //handle error
+            console.log(response);
+        })
+      },
     }
   }
 
--- pages/edit_taggeddata.vue
+++ pages/edit_taggeddata.vue
@@ -1,50 +1,66 @@
 <template>
   <div>
-    <b-form @submit="saveChanges">
-      <b-form-group id="input-group-1" label=ANIMAL_CURRENTMARKER label-for="input-1">
-        <b-form-input
-          id="input-1"
-          :placeholder="item.field_data.ANIMAL_CURRENTMARKER"
-        ></b-form-input>
-      </b-form-group>
+    <b-card bg-variant="light">
+      <b-form-group>
+        <b-form @submit="saveChanges" v-model=form>
 
-      <b-form-group id="input-group-2" label="ANIMAL_IDENTIFYINGMARKERSTARTDATE" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          :placeholder="item.field_data.ANIMAL_IDENTIFYINGMARKERSTARTDATE"
-        ></b-form-input>
-      </b-form-group>
+          <b-form-group id="Title" :label="'Editing Animal ID: ' + item.animal_id"></b-form-group>
+          
+          <b-form-group id="input-group-1" label=SPECIES label-for="input-1">
+            <b-form-input
+              id="input-1"
+              :value="item.species"
+            ></b-form-input>
+          </b-form-group>
+          
+          <div v-for="(value, name) in item.field_data">
+            <b-form-group id="input-group-3" :label="`${name}`" label-for="input-3">
+              <b-form-input
+                id="input-3"
+                :value="`${value}`"
+              ></b-form-input>
+            </b-form-group>
+          </div>
 
-      <b-form-group id="input-group-3" label="ANIMAL_ORIGINALMARKER" label-for="input-3">
-        <b-form-input
-          id="input-3"
-          :placeholder="item.field_data.ANIMAL_ORIGINALMARKER"
-        ></b-form-input>
-      </b-form-group>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
-    </b-form>
+          <b-button type="submit" variant="primary">Submit</b-button>
+          <b-button type="button" variant="primary" @click="$router.go(-1)">Cancel</b-button>
+        </b-form>
+      </b-form-group>
+    </b-card>
+
   </div>
 </template>
 
 <script>
   export default {
-    computed: {
-      item() {
-        return this.$store.state.animals.activeItem
-      },
+    data() {
+      return {
+        item: this.$store.state.animals.activeItem,
+        form: {
+          SPECIES: '',
+          ANIMAL_CURRENTMARKER: '',
+          ANIMAL_IDENTIFYINGMARKERSTARTDATE: '',
+          ANIMAL_ORIGINALMARKER: ''
+        }
+      }
     },
     methods: {
-      saveChanges() {
+      saveChanges(evt) {
+        evt.preventDefault()
         // TODO: add axios patch to submit edits
         this.$axios({
           url: this.item.url,
-          method: 'PATCH',
-          data: fields,
+          method: 'patch',
+          data: this.$item,
           headers: {Authorization: this.$auth.$storage._state['_token.local']}
-        }).then((response) => {
-          // check if successful and reroute to taggeddata page
-          this.$router.push('taggeddata')
+        }).then(function (response) {
+            //this.$router.push('taggeddata')
+            console.log(response);
+        })
+        .catch(function (response) {
+            //handle error
+            console.log(response);
         })
       }
     }
