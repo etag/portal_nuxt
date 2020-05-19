73adaea7be73c184ca49b876968fe3545569d639
Edsel Brian Norwood II <edselnorwood83@gmail.com>
2020-05-19 19:32:50 -0400

    added coordinate inputs and change code so placeholders are used if no input

--- pages/edit_readerdata.vue
+++ pages/edit_readerdata.vue
@@ -23,11 +23,19 @@
           </b-form-group>
 
           <b-form-group id="input-group-3" label="Start Timestamp" label-for="input-3">
-            <b-form-input id="input-3" v-model="form.cor_lat" :placeholder="timestamp.start"></b-form-input>
+            <b-form-input id="Time_s" v-model="form.time_s" :placeholder="timestamp.start"></b-form-input>
           </b-form-group>
 
           <b-form-group id="input-group-4" label="End Timestamp" label-for="input-4">
-            <b-form-input id="input-4" v-model="form.cor_lon" :placeholder="timestamp.end"></b-form-input>
+            <b-form-input id="Time_e" v-model="form.time_e" :placeholder="timestamp.end"></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="input-group-5" label="Longitude" label-for="input-5">
+            <b-form-input id="Cor_lat" v-model="form.cor_lat" :placeholder="coordinates.long"></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="input-group-6" label="Latitude" label-for="input-6">
+            <b-form-input id="Cor_long" v-model="form.cor_lon" :placeholder="coordinates.lat"></b-form-input>
           </b-form-group>
 
           <b-button type="submit" variant="primary">Submit</b-button>
@@ -54,6 +62,10 @@ export default {
       timestamp: {
         start: "",
         end: ""
+      },
+      coordinates: {
+        lat: "",
+        lon: ""
       }
     };
   },
@@ -81,6 +93,16 @@ export default {
           self.timestamp.start = "None";
           self.timestamp.end = "None";
         }
+        let { location_data } = self.$axios
+          .get(
+            "/api/etag/locations/?location_id=" +
+              reader_data.data.results[0].location_id +
+              "&format=json"
+          )
+          .then(function(location_data) {
+            self.coordinates.lat = location_data.data.results[0].latitude;
+            self.coordinates.long = location_data.data.results[0].longitude;
+          });
       });
   },
   methods: {
@@ -102,10 +124,38 @@ export default {
             description: self.form.Description,
             reader_id: self.form.reader_id
           };
+
           self.$store.commit("readers/update", payload);
-          console.log(result.data.results[0].location_id);
-          self.$axios
-            .patch("/api/etag/readers/" + result.data.results[0].reader_id, {
+
+          if (typeof self.form.Description == "undefined") {
+            self.form.Description = document
+              .getElementById("Description")
+              .getAttribute("placeholder");
+          }
+          if (typeof self.form.time_s == "undefined") {
+            self.form.time_s = document
+              .getElementById("Time_s")
+              .getAttribute("placeholder");
+          }
+          if (typeof self.form.time_e == "undefined") {
+            self.form.time_e = document
+              .getElementById("Time_e")
+              .getAttribute("placeholder");
+          }
+          if (typeof self.form.cor_lat == "undefined") {
+            self.form.cor_lat = document
+              .getElementById("Cor_lat")
+              .getAttribute("placeholder");
+          }
+          if (typeof self.form.cor_long == "undefined") {
+            self.form.cor_lon = document
+              .getElementById("Cor_long")
+              .getAttribute("placeholder");
+          }
+
+          self.$axios.patch(
+            "/api/etag/readers/" + result.data.results[0].reader_id,
+            {
               url: self.$store.state.readers.activeItem.url,
               reader_id: self.form.reader_id,
               description: self.form.Description,
@@ -113,10 +163,32 @@ export default {
               headers: {
                 Authorization: self.$auth.$storage._state["_token.local"]
               }
-            })
-            .then(function() {
-              self.$router.push("readerdata");
-            });
+            }
+          );
+
+          self.$axios.patch(
+            "/api/etag/reader_location/" + result.data.results[0].reader_id,
+            {
+              start_timestamp: self.form.time_s,
+              end_timestamp: self.form.time_e,
+              headers: {
+                Authorization: self.$auth.$storage._state["_token.local"]
+              }
+            }
+          );
+
+          self.$axios.patch(
+            "/api/etag/locations/" + result.data.results[0].location_id,
+            {
+              latitude: self.form.cor_lat,
+              longitude: self.form.cor_long,
+              headers: {
+                Authorization: self.$auth.$storage._state["_token.local"]
+              }
+            }
+          );
+
+          self.$router.push("readerdata");
         });
     }
   }
