15b1e1a3070099473db97710a1f9f12a8efdb092
Edsel Brian Norwood II <edselnorwood83@gmail.com>
2020-05-19 16:10:18 -0400

    update edit functionality

--- store/readers.js
+++ store/readers.js
@@ -1,42 +1,46 @@
 export const state = () => ({
-  list: [],     // list of records for current page
-  count: 0,     // total number of records
-  page: 1,      // track current page
+  list: [], // list of records for current page
+  count: 0, // total number of records
+  page: 1, // track current page
   pageSize: 20, // number of items to show per page
   activeItem: null, // object of active item used for editing
-  prev: null,   // url to previous page of content
-  next: null,   // url to next page of content
-})
+  prev: null, // url to previous page of content
+  next: null // url to next page of content
+});
 
 export const mutations = {
-  add (state, reader) {
-    state.list.push(reader)
+  update(state, payload) {
+    state.readers.activeItem.description = payload.description;
+    state.readers.activeItem.reader_id = payload.reader_id;
+  },
+  add(state, reader) {
+    state.list.push(reader);
   },
   appendList(state, readers) {
-    state.list = [].concat.apply(state.list, readers)
+    state.list = [].concat.apply(state.list, readers);
   },
   setList(state, readers) {
-    state.list = readers
+    state.list = readers;
   },
   setActiveItem(state, item) {
-    state.activeItem = item
+    state.activeItem = item;
   },
   setPage(state, page) {
-    state.page = page
+    state.page = page;
   },
   setPageSize(state, pageSize) {
-    state.pageSize = pageSize
+    state.pageSize = pageSize;
   },
   setCount(state, count) {
-    state.count = count
+    state.count = count;
   },
   setPrev(state, prev) {
-    state.prev = prev
+    state.prev = prev;
   },
   setNext(state, next) {
-    state.next = next
+    state.next = next;
   },
-  remove (state, { reader }) {
-    state.list.splice(state.list.indexOf(reader), 1)
+  remove(state, { reader }) {
+    state.list.splice(state.list.indexOf(reader), 1);
   }
-}
+};
