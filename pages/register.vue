1f73ba4d14d08b04bc294d679754998cc3491ad8
Tyler Pearson <tdpearson@ou.edu>
2020-05-21 16:44:09 -0500

    set register email subject

--- pages/register.vue
+++ pages/register.vue
@@ -2,7 +2,7 @@
   <div class="about text-left">
     <!-- FIXME: add instructions or form to register an account -->
     <h3>Site Registration</h3>
-    <p>Please email <a href="mailto:cmcurry@ou.edu" target="_blank">Claire Curry</a> to obtain a user account.  Provide your preferred username with a limit of 20 characters, no special characters.</p>
+    <p>Please email <a href="mailto:cmcurry@ou.edu?subject=Register ETAG account" target="_blank">Claire Curry</a> to obtain a user account.  Provide your preferred username with a limit of 20 characters, no special characters.</p>
   </div>
 </template>
 
